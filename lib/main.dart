09c6c642d071cbcacd9d7ff5cac5b423f224d372
Laith Shono <lshono8@gmail.com>
2020-09-22 19:01:05 +0300

    fixed app title

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'University Schedule Helper',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
         
